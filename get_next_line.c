4925177130bf482ac8503ed6ab4b6579b00ea26b
Thomas DA SILVA <tda-silv@e3r2p23.clusters.42paris.fr>
2022-04-02 17:29:12 +0200

    samedi 2 avril 2022 - push 1 -

---
+++ get_next_line.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tda-silv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 16:27:53 by tda-silv          #+#    #+#             */
+/*   Updated: 2022/04/02 16:27:55 by tda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+
+}
+
+int	main()
+{
+	int fd;
+	const char *filename = "fichier.txt";
+
+	fd = open(filename, O_RDWR):
+	return (0);
+}
